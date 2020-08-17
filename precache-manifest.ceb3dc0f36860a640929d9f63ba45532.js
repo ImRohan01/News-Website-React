5115faa6d3f06ede3539ba7ed000c00edc65e9f6
ImRohan01 <rhshetty01@gmail.com>
2020-08-17 14:10:49 +0530

    Updates

---
+++ precache-manifest.ceb3dc0f36860a640929d9f63ba45532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd41072b031fbc0f74632a3779d06c6",
+    "url": "/News-Website-React/index.html"
+  },
+  {
+    "revision": "f482a4702fa860bd6e02",
+    "url": "/News-Website-React/static/css/main.d5e1e51c.chunk.css"
+  },
+  {
+    "revision": "301355efe243edb08891",
+    "url": "/News-Website-React/static/js/2.604806cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/News-Website-React/static/js/2.604806cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f482a4702fa860bd6e02",
+    "url": "/News-Website-React/static/js/main.25537cad.chunk.js"
+  },
+  {
+    "revision": "ab5d8c92aafd69050a61",
+    "url": "/News-Website-React/static/js/runtime-main.710b589f.js"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/News-Website-React/static/media/spinner.235b9b3e.gif"
+  }
+]);
